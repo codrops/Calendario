097a4366bc82347c9f5d15b9949fd790c83435f3
deviprsd21 <deviprsd21@gmail.com>
2013-11-20 15:19:23 +0530

    Update jquery.calendario.js
    Calendario gets more flexible. Features like yearly events, monthly events, "today" event, whole month like vacations and onDayHover function helps in a more user friendly experience with more ease.
    
    Integrated some requests

--- js/jquery.calendario.js
+++ js/jquery.calendario.js
@@ -173,6 +173,7 @@
 						p = pos < 0 ? 6 + pos + 1 : pos,
 						inner = '',
 						today = this.month === this.today.getMonth() && this.year === this.today.getFullYear() && day === this.today.getDate(),
+						past = this.year < this.today.getFullYear() || this.month < this.today.getMonth() && this.year === this.today.getFullYear() || this.month === this.today.getMonth() && this.year === this.today.getFullYear() && day < this.today.getDate(),
 						content = '';
 					    
 					if ( day <= monthLength && ( i > 0 || j >= p ) ) {
@@ -251,6 +252,9 @@
 					}
 
 					var cellClasses = today ? 'fc-today ' : '';
+					if ( past ) {
+		              cellClasses += 'fc-past ';
+		            }
 					if( content !== '' ) {
 						cellClasses += 'fc-content';
 					}
