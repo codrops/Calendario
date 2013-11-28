eb3441c9b25c244294b932a49b2080bd1a1c1c04
deviprsd21 <deviprsd21@gmail.com>
2013-11-28 09:12:57 +0530

    Update data.js
    Some examples ...

--- js/data.js
+++ js/data.js
@@ -1,15 +1,16 @@
 var codropsEvents = {
 	'11-21-2013' : '<a href="http://www.wincalendar.com/Great-American-Smokeout">Great American Smokeout</a>',
-	'11-13-2013' : '<span>Ashura</span>',
+	'11-13-2013' : '<span>Ashura [An example of an complete date event (11-13-2013)]</span>',
 	'11-11-2013' : '<a href="http://www.wincalendar.com/Remembrance-Day">Remembrance Day (Canada)</a>',
 	'11-04-2013' : '<span>Islamic New Year</span>',
 	'11-03-2013' : '<a href="http://www.wincalendar.com/Daylight-Saving-Time-Ends">Daylight Saving Time Ends</a>',
 	'11-01-2013' : '<span>All Saints Day</span>',
-	'12-25-YYYY' : '<span>Christmas Day (Event repeats every YEAR)</span>',
-	'01-02-YYYY' : '<span>New Year\'s Eve (Event repeats every YEAR)</span>',
-	'MM-02-2013' : '<span>Monthly event in a year</span>',
-	'MM-07-YYYY' : '<span>Monthly event in all year</span>',
-	'11-DD-2014' : {content : '<span>Whole month events like (vacations) of a year</span>', end : 20},
-	'12-DD-YYYY' : '<span>Whole month events like (vacations) for future year</span>',
-	'TODAY' : '<span>Today</span>'
+	'12-25-YYYY' : '<span>Christmas Day [Date : 12-25-YYYY]</span>',
+	'01-01-YYYY' : '<span>New Year\'s Eve (Event repeats every YEAR)</span>',
+	'MM-02-2013' : '<span>Yeah, Monthly [MM-02-2013]</span>',
+	'MM-07-YYYY' : '<span>[MM-07-YYYY], Monthly and Yearly</span>',
+	'11-DD-2014' : {content : '<span>Ex: {\'11-DD-2014\' : {content : \'Something\', end : 20}}</span>', end : 20},
+	'02-DD-2014' : {content : '<span>Ex: {\'02-DD-2014\' : {content : \'Something\', start : 10, end : 20}}</span>', start : 10, end : 20},
+	'12-DD-YYYY' : '<span>[12-DD-YYYY] Whole month and Year</span>',
+	'TODAY' : '<span>Today, [Date : \'TODAY\']</span>'
 };
