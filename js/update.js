531730034c3dbc30d3e28d25e5eb506344321e96
Devi Prasad <deviprsd21@gmail.com>
2014-11-16 13:21:12 +0530

    Update update.js
    testing phase

--- js/update.js
+++ js/update.js
@@ -1,5 +1,5 @@
 var calendario = {
-	current: '3.1.0',
-	previous: '3.0.0',
-	download: 'https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.js'
+    current: '3.1.0',
+    previous: '3.0.0',
+    download: 'https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.js'
 };
