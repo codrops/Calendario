9f0ff9269fa5a21f377ae50aa841a9e6f0803dda
Devi Prasad <deviprsd21@gmail.com>
2014-11-16 15:20:47 +0530

    Create dataTimeline.js

---
+++ js/dataTimeline.js
@@ -0,0 +1,18 @@
+var timelineEvents = {
+	'TODAY': [
+		{content: '<span>John Applesed\'s Birthday</span>', allDay: true},
+		{content: '<span>Arnold Mathew\'s Birthday</span>', allDay: true},
+		{content: '<span>Captain X\'s Birthday</span>', allDay: true},
+		{content: '<span>Sleep, sleep, sleep!</span>', startTime: '00:00', endTime: '06:00'}, 
+		{content: '<span>Wake Up! Get Ready! 30 Mins Max :)</span>', startTime: '06:00', endTime: '06:30'},
+		{content: '<span>I\'m getting late for work! GOD!! (Average time 17mins to reach)</span>', startTime: '06:30', endTime: '06:47'},
+		{content: '<a href="https://en.wikipedia.org/wiki/LIFI">Make report on Li-Fi</a>', startTime: '06:47', endTime: '12:00'},
+		{content: '<span>Lunch with friend\'s at KFC</span>', startTime: '12:00', endTime: '13:00'}, 
+		{content: '<span>10mins break :p</span>', startTime: '13:00', endTime: '13:10'},
+		{content: '<span>Have no clue? O.o</span>', startTime: '13:20', endTime: '17:41'},
+		{content: '<span>Quick nap, after returning home!</span>', startTime: '18:00', endTime: '18:20'},
+		{content: '<span>Dinner with my Girl Friend</span>', startTime: '19:00', endTime: '21:30'},
+		{content: '<span>Prepare for Sleep!</span>', startTime: '22:00', endTime: '23:01'},
+		{content: '<span>Trying to sleep</span>', startTime: '23:01', endTime: '00:00'}
+	]
+};
