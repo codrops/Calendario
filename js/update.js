347c868cca0b97a533066530913bd7ecf68a3af7
Devi Prasad <deviprsd21@gmail.com>
2014-11-20 20:59:42 +0530

    v3.2.0

--- js/update.js
+++ js/update.js
@@ -1,10 +1,11 @@
 var calendario = {
-    current: '3.1.0',
-    previous: '3.0.0',
+    current: '3.2.0',
+    previous: '3.1.0',
     download: 'https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.js',
     msg: 'CALENDARIO MSG' +
          '\n===============' +
          '\nNEW VERSION (3.1.0) AVAILABLE!' +
          '\nDOWNLOAD : https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.js' +
-         '\n=============================================================================================='
+         '\nMINIFIED : https://raw.githubusercontent.com/codrops/Calendario/master/js/jquery.calendario.min.js' +
+         '\n=================================================================================================='
 };
